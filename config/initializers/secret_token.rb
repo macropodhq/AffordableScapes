94cb43fd1b6635954c05ef2824fa84dac8d04346
Alan Downie <alan@angrymonkeys.com.au>
2011-03-17 23:11:55 -0700

    sf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Affordable::Application.config.secret_token = 'd4ebf925f46a5b4e0abde9b1b27f268d5890dab44109691f24da034a0a5d9e8f825b0638f0394396c8ab6770b340ce35b15f0b72ecabf8105219bfb22c8dcac1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Affordable::Application.config.session_store :cookie_store, :key => '_Affordable_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Affordable::Application.config.session_store :active_record_store
